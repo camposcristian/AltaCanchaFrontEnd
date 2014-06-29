073c4bb83bf9043614489d36b9bf63bb73f10146
Axel <apompa1587@gmail.com>
2014-06-29 06:23:44 -0300

    Club detail y booking

--- src/app/clubs/clubsDetailController.js
+++ src/app/clubs/clubsDetailController.js
@@ -20,10 +20,29 @@ angular.module('clubsModule').controller('clubsDetailController', function ($sco
     $scope.court = club.Courts[0];
     $scope.$storage.court = $scope.court;
 
+    $scope.selectedHours = $scope.$storage.selectedHours;
+
     $scope.selectCourt = function (court) {
         $scope.court = court;
         $scope.$storage.court = court;
     };
+
+    $scope.h1 = "18:30";
+    $scope.h2 = "19:00";
+    $scope.h3 = "19:30";
+    $scope.h4 = "20:00";
+    $scope.h5 = "20:30";
+
+    $scope.selectedHours = [];
+    $scope.selectHour = function (hour) {
+        if( $scope.selectedHours.indexOf(hour) == -1 ){
+            $scope.selectedHours.push(hour);
+        }else{
+            $scope.selectedHours.splice($scope.selectedHours.indexOf(hour), 1);
+        }
+        $scope.$storage.selectedHours = $scope.selectedHours;
+    };
+
 
 
 });
